1e37696911a9d83a07228b6ac47e0cfd12f8f55f
Inceptix <m.rrapaj@gmail.com>
2020-02-16 19:07:56 -0500

    Finished Lab4 with comments and fixed warning :+1:

--- Paycheck and Revenue/Paycheck and Revenue/Arrays1DDemo.c
+++ Paycheck and Revenue/Paycheck and Revenue/Arrays1DDemo.c
@@ -1,11 +1,11 @@
 /*
 Marinos Rrapaj
-February 1, 2020
-CIS2107_Lab02_Paycheck_and_Revenue
+February 16, 2020
+CIS2107_Lab03_Processing1DArrays
 
-Statement: To write a program in C called paycheck that calculated the paycheck for a Temple employee
-based on their weekly hours and the rate per hour. The goal is also to figure out if the employee
-worked overtime (more than 40 hours) and multiply their hourly rate by x1.5 for any hours above 40.
+Statement: To write a program in C called Arrays1DDemo.c and implement different functionality to show
+proficiency in working with arrays. Some of the functions include being able to find a certain value
+within a range provided, being able to reverse an array, and also finding a given sequence of numbers.
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -41,6 +41,7 @@ int main()
 
 	int scan1, scan2;
 	scan1 = 0;
+	//implement an easy way of testing for sequence
 	while (scan1 != -1)
 	{
 		printf("Please input sequence to be found. Input -1 to quit.\n");
@@ -57,10 +58,13 @@ void fillArray(int x[])
 	int counter = 0;
 	for (size_t i = 0; i < ARRAY_SIZE; i++)
 	{
+		//this gives a random number for every index
 		x[i] = rand() % (RAND_MAX + 1) + RAND_MIN;
 	}
 
 	printf("Original Array:\n");
+
+	//custom function for printing the array similarly to the demo
 	printArray(x);
 
 	puts("");
@@ -69,6 +73,8 @@ void fillArray(int x[])
 
 void findWithRange(int x[])
 {
+	//declare max variable to be ther first given number in the range
+	//then check if any other numbers are bigger and reassign
 	int max = x[LOW];
 	for (size_t i = LOW; i <= HIGH; i++)
 	{
@@ -84,7 +90,10 @@ void findWithRange(int x[])
 
 void reverseArray(int x[])
 {
+	//revCounter basically acts as the reverse index for the new array
 	int revCounter = ARRAY_SIZE - 1;
+
+	//created a new array which is reversed
 	int revArray[ARRAY_SIZE] = { 0 };
 	for (size_t i = 0; i < ARRAY_SIZE; i++)
 	{
@@ -98,11 +107,14 @@ void reverseArray(int x[])
 
 void printArray(int* x)
 {
+	//custom print function
 	int counter = 0;
 	for (size_t i = 0; i < ARRAY_SIZE; i++)
 	{
 		printf("%5d", x[i]);
 		counter++;
+
+		//chaning this number will change the number of columns that appear on the screen
 		if (counter > 9)
 		{
 			puts("");
@@ -114,6 +126,9 @@ void printArray(int* x)
 void reverseSequence(int x[])
 {
 	int revCounter = HIGH;
+
+	//the array with a reversed sequence is also a separate array
+	//it uses the same logic for reversing the sequence as the function above
 	int revArray[ARRAY_SIZE] = { 0 };
 	for (size_t i = LOW; i <= HIGH; i++)
 	{
@@ -121,11 +136,13 @@ void reverseSequence(int x[])
 		revCounter--;
 	}
 
+	//fills the rest of the array with the same values as the original array
 	for (size_t i = 0; i < LOW; i++)
 	{
 		revArray[i] = x[i];
 	}
 
+	//fills the rest of the array with the same values as the original array
 	for (size_t i = HIGH+1; i < ARRAY_SIZE; i++)
 	{
 		revArray[i] = x[i];
@@ -137,16 +154,21 @@ void reverseSequence(int x[])
 
 void findSequence(int x[], int y, int z)
 {
+	//loop through array
 	for (size_t i = 0; i < ARRAY_SIZE; i++)
 	{
+		//chech if the index is equal to the value
+		//have to make sure to stay within the bounds of the array and not read data outside the array
 		if ((y == x[i]) && (i < ARRAY_SIZE-1))
 		{
+			//if the second value is also true then print message.
+			//in the case that there are multiple sequences that exist this message will print all those times
 			if (z == x[i + 1])
 			{
-				printf("sequence found at index %d\n", i);
-				return;
+				printf("sequence found at index %u\n", i);
 			}
 		}
 	}
+	//once we have finished scanning the whole array then print the message that sequence does not exist
 	printf("Sequence not found -1\n");
 }
